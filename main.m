a61082650e149adcaa13cdb7e5684fe7f39cbf15
minjong kim <alswhddkdl@gmail.com>
2025-03-27 23:42:49 +0900

    /

--- main.m
+++ main.m
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
 
+// 상수 정의
+#define LENGTH 10   
+#define WIDTH  5
+#define NEWLINE '\n'
+
 // 정의
 //  모든 객체의 기본 클래스인 NSObject를 상속합니다.
 @interface SampleClass: NSObject // - (void)sampleMethod;는 메서드를 선언하는 방법
@@ -15,9 +20,34 @@
 @end
 
 int main(int argc, const char * argv[]) {
+
+    // 특정 유형의 상수를 선언
+    const int a = 10;
+
+    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
    NSLog (@"hello world");
+  
+   // size of int
+   NSLog(@"Storage size for int : %d \n", sizeof(int));
+
+
+      /* variable definition: */
+  int a, b;
+  int c;
+  float f;
+ 
+  /* actual initialization */
+  a = 10;
+  b = 20;
+  
+  c = a + b;
+  NSLog(@"value of c : %d \n", c);
+
+  f = 70.0/3.0;
+  NSLog(@"value of f : %f \n", f);
+
    [pool drain];
    return 0;
 }

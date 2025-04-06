2bf7b8f249a3b3bf2c8cae1ab26bc54ec22ac519
minjong kim <alswhddkdl@gmail.com>
2025-04-06 22:38:40 +0900

    /

--- dog/main.m
+++ dog/main.m
@@ -5,10 +5,15 @@ int main() {
     // 인스턴스 생성성
     Dog *happy = [[Dog alloc] init];
     // 메소드 호출
-    [happy setAge: 5];
-    NSLog(@"%d",[happy age]);
+    // [happy setAge: 5];
+    
+    // synthesize를 사용하면 dot으로 접근 가능
+    happy.age = 5; // 세터 동작
 
-    [happy setAge: 2];
+    // NSLog(@"%d",[happy age]);
+    NSLog(@"%d", happy.dot); // 게터 동작
+
+    // [happy setAge: 2];
     NSLog(@"%d",[happy age]);
 
     [happy show];

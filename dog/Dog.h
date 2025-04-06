56e8b27035b296dcadf3941905cedaa70f310dcc
minjong kim <alswhddkdl@gmail.com>
2025-04-06 22:30:55 +0900

    practice 3 - property&synthesize instead of getter&setter

--- dog/Dog.h
+++ dog/Dog.h
@@ -4,8 +4,12 @@
 {
     int age;
 }
--(void) setAge: (int) a;
-// 옵씨에서는 보통 getter에 get을 안씀
--(int) age;
+// -(void) setAge: (int) a;
+// // 옵씨에서는 보통 getter에 get을 안씀
+// -(int) age;
+
+// @property로 한번에 선언
+@property int age;
+
 -(void) show;
 @end
--- dog/Dog.m
+++ dog/Dog.m
@@ -1,15 +1,19 @@
 #import "Dog.h"
 
 @implementation Dog
--(void) setAge: (int) a
-{
-    age = a;
-}
+// -(void) setAge: (int) a
+// {
+//     age = a;
+// }
 
--(int) age
-{
-    return age;
-}
+// -(int) age
+// {
+//     return age;
+// }
+
+// @property로 한번에 선언
+// @synthesize로 정의 부분에 성의
+@synthesize age;
 
 -(void) show
 {
